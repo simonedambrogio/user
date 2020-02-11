aa44fdff86cb2e13e7813aa31e4138967a67df4f
Simone <simonedambrgio@live.it>
2020-02-10 19:26:28 -0500

    make consecutive trials

---
+++ R/trials.R
@@ -0,0 +1,16 @@
+#Trasforma i tril in trial consecutivi 
+trials <- function(data) {
+  
+  if( sum(names(data) %in% c("subject", "trial")) < 2 ){ "The dataset must contain the variables subject and trial"}
+  
+  #total number of trials
+  tot_trials <- sapply(unique(data$subject), function(sbj)  length(unique(data[data$subject == sbj, "trial"])) ) %>% sum()
+  
+  #Create a variable trial  
+  trials <- rep(1:tot_trials, 
+                data %>% 
+                  group_by(subject, trial) %>% 
+                  summarise(n()) %>% .[, 3] %>% 
+                  unlist() %>% as.double())
+  return(trials)
+}
