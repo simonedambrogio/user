b5da9e7ab9ff6a87f5c46ba2050193e88c19eaf9
Simone <simonedambrgio@live.it>
2020-05-22 10:36:15 -0400

    up

--- R/trials.R
+++ R/trials.R
@@ -1,7 +1,7 @@
 trials <- function(x){
   idx <- c(0, which(diff(x)<0), length(x))
   lapply(1: c(length(idx)-1), function(i){ 
-    tr_i <- tr[ (idx[i]+1) : idx[i+1]]
-    rep(1:length(unique(tr_i)), rle(tr_i)$lengths)
+    x_i <- x[ (idx[i]+1) : idx[i+1]]
+    rep(1:length(unique(x_i)), rle(x_i)$lengths)
   }) %>% unlist
 }
