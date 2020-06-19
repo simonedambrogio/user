8a8a2386be80f50111d085c9c3eded8debbbb659
Simone <simonedambrgio@live.it>
2020-06-19 12:19:48 -0400

    up

--- R/words_rm.R
+++ R/words_rm.R
@@ -3,10 +3,11 @@
 #data: name of the dataset
 #variable: character variable that we want to modify. don't forget the quotation marks
 #words_to_rm: words that we want to eliminate separated by |
-#replace: numbers or words we want instead of words_rm. Default is "" and you should 
+#replace_with: numbers or words we want instead of words_rm. Default is "" and you should 
 #keep it if you want to eliminate the words
 
 words_replace <- function (data, variable, words_to_replace, replace_with = "") {
+  
   
   library(dplyr)
   if (!require(dplyr)) {
@@ -19,12 +20,16 @@ words_replace <- function (data, variable, words_to_replace, replace_with = "") 
   
   if(!is.character(var)) var <- as.character(var)
   
-  for (word_i in 1:length(words_to_rm)) {
-    word_to_rm_logic <- str_detect(var, fixed(words_to_rm[word_i]))
+  for (word_i in 1:length(words_to_replace)) {
+    word_to_rm_logic <- str_detect(var, fixed(words_to_replace[word_i]))
     
     if(any(word_to_rm_logic)) {
-      word_to_rm <- words_to_rm[word_i]
-      new_words <- sapply(1:sum(word_to_rm_logic), function(i) var[word_to_rm_logic][i] <- gsub(word_to_rm, replace, var[word_to_rm_logic][i], fixed = TRUE))
+      words_to_replace_i <- words_to_replace[word_i]
+      
+      new_words <- sapply(1:sum(word_to_rm_logic), function(i){
+        var[word_to_rm_logic][i] <- gsub(words_to_replace_i, replace_with, var[word_to_rm_logic][i], fixed = TRUE)
+      })
+      
       var[word_to_rm_logic] <- new_words
     } else {
       warning( paste0("Sting '",  words_to_replace ,"' not found") )
