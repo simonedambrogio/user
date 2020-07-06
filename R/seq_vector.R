ac2cf04efd11771bc6ee509fd1dc67536d0455f2
Simone <simonedambrgio@live.it>
2020-07-06 12:57:45 -0400

    usa funzione rep!

--- R/seq_vector.R
+++ R/seq_vector.R
@@ -1,7 +1,5 @@
 seq_vector <- function(x){
-  idx <- c(0, which(diff(x)<0), length(x))
-  lapply(1: c(length(idx)-1), function(i){ 
-    x_i <- x[ (idx[i]+1) : idx[i+1]]
-    rep(1:length(unique(x_i)), rle(x_i)$lengths)
-  }) %>% unlist
+  idx <- c(which(diff(x) != 0), length(x))
+  time <- c(idx[1], diff(idx))
+  mapply(rep, x=1:length(idx), times=time) %>% unlist
 }
