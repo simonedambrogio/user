53a5b6e011b5519fd8e1b2bbee036fadffb7241e
Simone <simonedambrgio@live.it>
2021-03-08 10:58:54 +0100

    make it better

--- R/seq_vector.R
+++ R/seq_vector.R
@@ -1,5 +1,7 @@
 seq_vector <- function(x){
   idx <- c(which(diff(x) != 0), length(x))
   time <- c(idx[1], diff(idx))
-  mapply(rep, x=1:length(idx), times=time) %>% unlist
+  m=mapply(rep, x=1:length(idx), times=time) %>% unlist
+  if(is.matrix(m)) m=as.vector(matrix(m, ncol = 1))
+  return(m)
 }
